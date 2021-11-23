47e6fd8b56dee0dcaa8aaefc88a0d278d5bfb5da
GustavORios <gustavo.rios9@hotmail.com>
2021-11-22 21:52:28 -0300

    Migração DB

---
+++ API/Migrations/20211123003305_Initial.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "jogos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Genero = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Plataforma = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CriadoEm = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_jogos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "perfils",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    idJogo = table.Column<int>(type: "int", nullable: false),
+                    eloLOL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    champ = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    laneMain = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    laneSecundaria = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CriadoEm = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_perfils", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Idade = table.Column<int>(type: "int", nullable: false),
+                    Pais = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CriadoEm = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuarios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "jogos");
+
+            migrationBuilder.DropTable(
+                name: "perfils");
+
+            migrationBuilder.DropTable(
+                name: "usuarios");
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -42,7 +42,7 @@ namespace API
 
             //Configuração do serviço de banco de dados
             services.AddDbContext<DataContext>(
-                options => options.UseInMemoryDatabase("database")
+                options => options.UseSqlServer(Configuration.GetConnectionString("Azure"))
             );
             services.AddControllers();
             services.AddSwaggerGen(c =>
